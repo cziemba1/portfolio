3c95b953f0e7e2885a0060e99b5b3888ba4db077
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-31 22:24:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const nav = $("#nav");
-const intro = $("#intro");
 var tl = gsap.timeline();
 
-tl.from("#nav", 2, { autoAlpha: 0 })
+tl.from("#nav", 2, { autoAlpha: 0, ease: "power2.in" })
+    .staggerFrom(".ani-intro", 1, { autoAlpha: 0, y: 10, ease: "power2.in" }, 0.4, "-=1")
+    .staggerTo(".underline", 1, { textDecoration: "underline", ease: "power2.in", delay: .5 }, 0.2)
 
 
 
