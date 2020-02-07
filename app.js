dfbe4aa5464fb3647513b32f25fee4002470a3f7
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-02-07 00:23:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 gsap.from("#nav", 2, { opacity: 0, ease: "power1.out" });
-gsap.from(".ani-intro", 1.8, { autoAlpha: 0, ease: "power1.out", delay: 1, stagger: 0.4 });
+gsap.from(".ani-intro", 1.8, { autoAlpha: 0, ease: "power1.out", delay: .7, stagger: 0.4 });
 gsap.to(".underline", 1, { textDecoration: "underline", ease: "power1.out", delay: .5, stagger: 0.2 })
 gsap.from(".intro-first", 1.9, { autoAlpha: 0, ease: "power1.out", delay: 2 })
 
